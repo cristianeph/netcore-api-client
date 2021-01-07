baef69619c0170b2d37fede4bc70cc324e308a1a
Cristian Peralta <cristianeph@me.com>
2021-01-07 05:00:25 -0500

    Actualizando urls de entorno y corrigiendo componentes

---
+++ src/app/shared/components/layout-form-actions/layout-form-actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-form-actions',
+  templateUrl: './layout-form-actions.component.html',
+  styleUrls: ['./layout-form-actions.component.css']
+})
+export class LayoutFormActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
